f4f4ba64cbbd5e5903164f569cfe4bda4747afe8
Deepak <deepaklewiskumar.19@gmail.com>
2025-09-05 02:42:59 +0530

    login-popup(UI only)

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navLinks = document.getElementById('navLinks');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+const loginpopup=document.getElementById("loginPopup");
+
+const btn=document.querySelector("#loginbtn");
+function openpopup(login){
+    login.style.display="block";
+}
+btn.onclick=()=>openpopup(loginpopup);
+
+function handlelogin(){
+  loginpopup.classList.remove("hidden");
+}
